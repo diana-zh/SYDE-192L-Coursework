e977c6503dae53d7a7ea4f163b04baff21b79ec4
Diana Zhang <dianazhang307@gmail.com>
2022-08-09 14:38:05 -0400

    Update Lab_2_4_3.vhd

--- Lab 2/Lab_2_4_3.vhd
+++ Lab 2/Lab_2_4_3.vhd
@@ -1,6 +1,5 @@
-library ieee;					-- Declare that you want to use IEEE libraries 
-use ieee.std_logic_1164.all;	-- Library for standard logic circuits 
--- use ieee.numeric_std.all; 	-- Another useful library for UNSIGNED numbers 
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity Lab_2_4_3 is 	-- entity definition 
 	port( KEY: 		in 	std_logic_vector(3 downto 0); 	-- Push buttons 
