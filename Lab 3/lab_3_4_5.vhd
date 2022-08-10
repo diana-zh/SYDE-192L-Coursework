de5a4ad60d17a29981fe11f9c06eec37594b2698
Diana Zhang <dianazhang307@gmail.com>
2022-08-10 18:12:09 -0400

    Update lab_3_4_5.vhd

--- Lab 3/lab_3_4_5.vhd
+++ Lab 3/lab_3_4_5.vhd
@@ -1,33 +1,37 @@
+-- Implements ALU system which computes four-bit addition and muliplication
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity lab_3_4_5 is 
-	port( SW: in std_logic_vector(9 downto 0);
-			HEX0:		out	std_logic_vector(6 downto 0);
-			HEX1:		out	std_logic_vector(6 downto 0);
-			HEX2:		out	std_logic_vector(6 downto 0);
-			HEX3:		out	std_logic_vector(6 downto 0)); 
+	port (
+		SW: 	in	std_logic_vector(9 downto 0);
+		HEX0:	out	std_logic_vector(6 downto 0);
+		HEX1:	out	std_logic_vector(6 downto 0);
+		HEX2:	out	std_logic_vector(6 downto 0);
+		HEX3:	out	std_logic_vector(6 downto 0)
+	); 
 	end entity; 
 
-
 architecture RTL of lab_3_4_5 is 
-signal x0, x1, x2, x3, y0, y1, y2, y3: std_logic; -- two 4 bit inputs x, y
-signal s4, s3, s2, s1, s0: 				std_logic; -- four bit adder outputs
-signal p7, p6, p5, p4, p3, p2, p1, p0: std_logic; -- four bit multiplier output
-signal r7, r6, r5, r4, r3, r2, r1, r0: std_logic; -- final alu outputs
+signal x0, x1, x2, x3, y0, y1, y2, y3:	std_logic; -- two 4 bit inputs x, y
+signal s4, s3, s2, s1, s0:		std_logic; -- four bit adder outputs
+signal p7, p6, p5, p4, p3, p2, p1, p0:	std_logic; -- four bit multiplier output
+signal r7, r6, r5, r4, r3, r2, r1, r0:	std_logic; -- final alu outputs
 signal sw_status : std_logic; 
 
 component  four_bit_adder is 
-	port (a0, a1, a2, a3, b0, b1, b2, b3:	in  std_logic; 
-			p0, p1, p2, p3, p4: 					out std_logic
-			);
+	port (
+		a0, a1, a2, a3, b0, b1, b2, b3:	in  std_logic; 
+		p0, p1, p2, p3, p4: 		out std_logic
+	);
 end component; 
 
 component four_bit_multiplier is 
-	port ( x0,x1,x2,x3:   in	std_logic; -- 4 bit input
-			 y0,y1,y2,y3: 	in std_logic;
-			 p0, p1, p2, p3, p4, p5, p6, p7: out std_logic); 
+	port (
+		x0,x1,x2,x3:   			in	std_logic; -- 4 bit input
+		y0,y1,y2,y3: 			in	std_logic;
+		p0, p1, p2, p3, p4, p5, p6, p7: out	std_logic); 
 end component; 
 
 begin 
