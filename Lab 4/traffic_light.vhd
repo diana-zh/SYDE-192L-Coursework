17b12a5f0fb8494e258fc3bc8e63b9b56ca5138f
Diana Zhang <dianazhang307@gmail.com>
2022-08-10 18:23:18 -0400

    Update traffic_light.vhd

--- Lab 4/traffic_light.vhd
+++ Lab 4/traffic_light.vhd
@@ -1,35 +1,37 @@
+-- Implements a traffic light state machine at an intersection
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity traffic_light is 
-	port(	LEDR: out std_logic_vector(7 downto 0);
-			LEDG: out std_logic_vector(7 downto 0);
-			HEX0: 	out std_logic_vector(6 downto 0);
-			HEX3: 	out std_logic_vector(6 downto 0);
-			CLOCK_50_B5B: 	in std_logic);
+	port (
+		LEDR: 		out 	std_logic_vector(7 downto 0);
+		LEDG:		out 	std_logic_vector(7 downto 0);
+		HEX0:		out 	std_logic_vector(6 downto 0);
+		HEX3:		out 	std_logic_vector(6 downto 0);
+		CLOCK_50_B5B: 	in 	std_logic);
 			
 end entity;
 
 architecture RTL of traffic_light is
 
-type traffic_states is (NS_green_f, NS_green_s, NS_yellow, NS_red,
-								EW_green_f, EW_green_s, EW_yellow, EW_red);
+type traffic_states is (NS_green_f, NS_green_s, NS_yellow, NS_red, EW_green_f, EW_green_s, EW_yellow, EW_red);
 								
 						
 signal state, next_state:		traffic_states;
-signal state_num: 		unsigned(2 downto 0);	
-signal counter_Hz:		unsigned(21 downto 0);
-signal state_counter: 	unsigned(3 downto 0);
-signal helper_counter: 				unsigned(4 downto 0);			
-signal clock_10_Hz: 		std_logic;
-signal final_clock: 		unsigned(3 downto 0);
+signal state_num: 			unsigned(2 downto 0);	
+signal counter_Hz:			unsigned(21 downto 0);
+signal state_counter: 			unsigned(3 downto 0);
+signal helper_counter: 			unsigned(4 downto 0);			
+signal clock_10_Hz: 			std_logic;
+signal final_clock: 			unsigned(3 downto 0);
 
 
 component seven_segment is 
-	port (data_in: 	  in  std_logic_vector(3 downto 0); 
-			blanking: 	  in  std_logic;
-			segments_out: out std_logic_vector(6 downto 0));
+	port (
+		data_in: 	  	in  std_logic_vector(3 downto 0); 
+		blanking: 	  	in  std_logic;
+		segments_out:		out std_logic_vector(6 downto 0));
 end component; 
 
 begin
@@ -81,7 +83,6 @@ begin
 		when NS_yellow =>
 			LEDG(7) <= '0';
 			LEDR(0) <= clock_10_Hz;
-			-- LEDR(4) <= '1';
 			state_num <= to_unsigned(2, 3);
 			
 			if state_counter = 3 then
@@ -149,16 +150,6 @@ end process;
 hex_inst_1: entity work.seven_segment(behavioral) port map('0' & std_logic_vector(state_num), '0', HEX3);
 hex_inst_4: entity work.seven_segment(behavioral) port map(std_logic_vector(state_counter), '0', HEX0);
 
-
-end architecture;
-		
-			
-	
-	
-	
-	
-	
-	
-	
+end architecture;	
 	
 
