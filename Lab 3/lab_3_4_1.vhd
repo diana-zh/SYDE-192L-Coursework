62dff1c7ea8bfa729696bd6f703cf132d7590f38
Diana Zhang <dianazhang307@gmail.com>
2022-08-10 17:55:22 -0400

    Update lab_3_4_1.vhd

--- Lab 3/lab_3_4_1.vhd
+++ Lab 3/lab_3_4_1.vhd
@@ -1,18 +1,19 @@
+-- Implements an elevator controller onto an FPGA board
 library ieee;
 use ieee.std_logic_1164.all;
 
 
 entity lab_3_4_1 is 	-- entity definition 
 	port( 
-			SW: 		in 	std_logic_vector(3 downto 0); 	-- Toggle switches 
-			LEDR:		out 	std_logic_vector(1 downto 0); 	-- Red LEDs 
-			LEDG:		out 	std_logic_vector(1 downto 0)		-- Green LEDs 
+		SW: 		in 	std_logic_vector(3 downto 0); 	-- Toggle switches 
+		LEDR:		out 	std_logic_vector(1 downto 0); 	-- Red LEDs 
+		LEDG:		out 	std_logic_vector(1 downto 0)	-- Green LEDs 
 			); 
 end entity lab_3_4_1; 
 
 architecture main of lab_3_4_1 is 
-signal a, b, x, y:	std_logic;	-- Naming inputs 
-signal enable, direction: 		std_logic;	-- AND gate output for reading by XOR gate  
+signal a, b, x, y:		std_logic;	-- Naming inputs 
+signal enable, direction: 	std_logic;	-- AND gate output for reading by XOR gate  
 
 begin 
 a <= SW(3); 
@@ -25,4 +26,4 @@ direction <= (a and not(b) and x and y) or (not(a) and b and x);
 
 LEDR(0) <= enable; 
 LEDG(0) <= direction;
-end architecture main; 
+end architecture main;
