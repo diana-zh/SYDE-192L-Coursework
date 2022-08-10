9ef629e9e96d39c63d1c787698c44780984cd433
Diana Zhang <dianazhang307@gmail.com>
2022-08-10 18:20:48 -0400

    Update and rename Random_num_gen.vhd to random_num_gen.vhd

--- Lab 4/random_num_gen.vhd
+++ Lab 4/random_num_gen.vhd
@@ -1,14 +1,16 @@
+-- Implements a random number generator by defining an eight-bit signal and incrementing by the fastest clock pulse on FPGA board
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity Random_num_gen is 
-	port(
-			KEY: in std_logic_vector(1 downto 0);
-			HEX0, HEX1: out std_logic_vector(6 downto 0);
-			HEX2, HEX3: out std_logic_vector(6 downto 0);
-			CLOCK_50_B5B: in std_logic);
-	end entity; 
+	port (
+		KEY: 		in	std_logic_vector(1 downto 0);
+		HEX0, HEX1:	out	std_logic_vector(6 downto 0);
+		HEX2, HEX3:	out	std_logic_vector(6 downto 0);
+		CLOCK_50_B5B:	in	std_logic);
+	);
+end entity; 
 
 architecture main of Random_num_gen is
 
@@ -17,12 +19,11 @@ signal h_0, h_1, h_2, h_3, h_4: std_logic_vector(3 downto 0);
 signal blank: std_logic:='1'; -- blanking by default
 signal first_blank: std_logic:='1';
 
-component seven_segment is port(
-
+component seven_segment is port (
 	data_in: in std_logic_vector(3 downto 0); 
 	blanking: in std_logic; 
-	segments_out: out std_logic_vector(6 downto 0) ); 
-
+	segments_out: out std_logic_vector(6 downto 0)
+	); 
 end component;
 
 begin
@@ -42,7 +43,5 @@ hex_component_1: entity work.seven_segment(behavioral) port map(h_0, blank, HEX0
 hex_component_2: entity work.seven_segment(behavioral) port map(h_1, blank, HEX1);
 hex_component_3: entity work.seven_segment(behavioral) port map(h_2, first_blank, HEX2);
 hex_component_4: entity work.seven_segment(behavioral) port map(h_3, first_blank, HEX3);
-
- 
 
 end architecture; 
